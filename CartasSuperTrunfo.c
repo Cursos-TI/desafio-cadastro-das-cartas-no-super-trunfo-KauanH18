73970c3c6daf953bad5bb636f011c80cb19d44ac
Kauan <kauanhenrirb@gmail.com>
2025-01-21 18:19:46 +0000

    new comimt - erro - nome da cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,45 @@
 //Teste larissa
 
 int main() {
+    char nome[100];
+    char codigo_carta[3];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+//Apresentação
+    printf("Bem-Vindo ao Super Trufo! \n"); 
+
+//Pergunta os dados e armazena-os 
+    printf("Digite o nome da cidade: "); 
+    scanf(" %s", &nome); 
+
+    printf("Digite o codigo da carta (ex: 01, 02): ");
+    scanf(" %s", &codigo_carta);
+
+    printf("Digite a população da cidade (sem vírgulas): ");
+    scanf(" %d", &populacao);
+
+    printf("Digite a área da cidade (em km²): ");
+    scanf(" %f", &area);
+
+    printf("Digite o PIB da cidade (em bilhões de reais): ");
+    scanf(" %f", &pib);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf(" %d", &pontos_turisticos);
+
+//Mostra os Dados da carta
+printf("\nDados cadastrados: \n");  
+printf("Nome: %s\n", nome);
+printf("Código da Carta: %s\n", codigo_carta);
+printf("População: %d\n", populacao);
+printf("Área: %f km²\n", area);
+printf("PIB: %g bilhões de reais\n", pib);
+printf("Número de pontos Turísticos: %d\n", pontos_turisticos);
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
